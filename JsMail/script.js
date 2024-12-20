c1633473acc3f426179bcc8757896079f5112ccc
Mirko <mirkocaccavone03@gmail.com>
2024-12-20 15:28:32 +0100

    [feat] add function, add output

--- JsMail/script.js
+++ JsMail/script.js
@@ -6,3 +6,45 @@
 // Non è consentito usare nessun metodo proprio degli array (come includes, per esempio).
 // Si può fare? Certo che si basta ragionare un po’.
 // Nota: Non è necessario provvedere alla validazione delle email
+
+
+
+// creare un array con le mail
+const listaMail = [
+    "utente1@gmail.com",
+    "utente2@gmail.com",
+    "utente3@gmail.com",
+    "utente4@gmail.com",
+    "utente5@gmail.com",
+    "utente6@gmail.com",
+    "utente7@gmail.com",
+    "utente8@gmail.com",
+];
+console.log(listaMail);
+
+// inserimento mail da parte dell'utente
+let mailUtente = prompt("inserisci la tua mail");
+console.log(mailUtente);
+
+
+// variabile mail corretta
+let mailCorretta = false;
+
+
+// ciclo per controllare se la mail è nella lista
+for (let i = 0; i <= listaMail.length; i++){
+
+    if(listaMail[i] === mailUtente){
+        mailCorretta = true;
+    }
+}
+
+// messaggio di output con messaggio se può accedere o no
+
+   if (mailCorretta === true) {
+    console.log("benvenuto alla fasta", mailUtente);
+
+   }else{
+    console.log("non puoi accedere alla festa", mailUtente);
+    
+}
